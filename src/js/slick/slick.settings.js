485d38eaddae669d73da250195286f4c8221c039
Nikita <nikitaaa.dr@gmail.com>
2021-08-03 18:49:47 +0300

    uservideos block

---
+++ src/js/slick/slick.settings.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        // dots: false,
+        // infinite: true,
+        // speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+        // prevArrow: $('.arrows__left'),
+        // nextArrow: $('.arrows__right'),
+    });
+});
