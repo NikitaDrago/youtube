bd618c4ba1f3082d95d2231cd4e252d7e4f06f4c
Nikita <nikitaaa.dr@gmail.com>
2021-08-05 02:02:06 +0300

    added uservideos block

--- src/js/slick/slick.settings.js
+++ src/js/slick/slick.settings.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
     $('.slider').slick({
-        // dots: false,
-        // infinite: true,
-        // speed: 300,
+        infinite: true,
+        speed: 300,
         slidesToShow: 6,
         slidesToScroll: 1,
-        arrows: true,
-        // prevArrow: $('.arrows__left'),
-        // nextArrow: $('.arrows__right'),
+        variableWidth: true,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
     });
 });
