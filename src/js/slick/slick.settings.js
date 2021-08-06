7e23df916777dcfd2794ff456d289d93d2879aff
Nikita <nikitaaa.dr@gmail.com>
2021-08-06 16:13:17 +0300

    recommended block

--- src/js/slick/slick.settings.js
+++ src/js/slick/slick.settings.js
@@ -5,7 +5,17 @@ $(document).ready(function(){
         slidesToShow: 6,
         slidesToScroll: 1,
         variableWidth: true,
-        prevArrow: $('.arrows__left'),
-        nextArrow: $('.arrows__right'),
+        prevArrow: $('#arrows__left'),
+        nextArrow: $('#arrows__right'),
+    });
+
+    $('.rec_slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: $('#rec__arrows_left'),
+        nextArrow: $('#rec__arrows_right'),
     });
 });
